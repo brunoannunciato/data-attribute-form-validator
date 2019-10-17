07a5e60391e1c77e18e9a83a30f7388cc182b7b1
Bruno Barboza Annunciato <bruno.annunciato@grupopan.com>
2019-10-17 10:35:41 -0300

    add required html validation

--- js/app.js
+++ js/app.js
@@ -5,28 +5,41 @@ fields.forEach(field => {
 	field.setAttribute('show-error', 0)
 })
 
-const throwError = event => {
+const validateRequiredFields = event => {
 	const element = event.target
-	const errorElement = document.createElement('span')
-	errorElement.classList.add('form-error')
-	errorElement.innerHTML = element.getAttribute('data-error')
 
+	console.log(element.getAttribute('required'))
 
-	if (element.getAttribute('data-error') && !element.value) {
-		element.setAttribute('show-error', parseInt(element.getAttribute('show-error')) + 1)
+	if (element.getAttribute('data-error') && !element.value && element.getAttribute('required') !== null) {
+		handleError.throwError(event)
 	} else {
-		const error = element.parentNode.querySelectorAll('.form-error');
-		element.setAttribute('show-error', 0)
-		error[0] && error[0].remove()
+		handleError.clearError(event)
 	}
+}
 
-	element.getAttribute('show-error') == 1 && element.parentElement.appendChild(errorElement)
+const handleError = {
+	throwError: event => {
+		const element = event.target
+		const errorElement = document.createElement('span')
+		errorElement.classList.add('form-error')
+		errorElement.innerHTML = element.getAttribute('data-error')
+	
+		element.setAttribute('show-error', parseInt(element.getAttribute('show-error')) + 1)
+	
+		element.getAttribute('show-error') == 1 && element.parentElement.appendChild(errorElement)
+	},
+	clearError: event => {
+		const element = event.target
+		const error = element.parentNode.querySelectorAll('.form-error')
+			element.setAttribute('show-error', 0)
+			error[0] && error[0].remove()
+	}
 }
 
 const validateField = () => {
 	fields.forEach( field => {
 		field.addEventListener('blur', event => {
-			throwError(event)
+			validateRequiredFields(event)
 		})
 	})
 }
